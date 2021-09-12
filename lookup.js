c00b7f7a18004f15aec1ec0e90e1db6c3011ef62
Ravil Galeyev <dehasi@yandex.ru>
2021-09-13 01:50:21 +0200

    X

--- lookup.js
+++ lookup.js
@@ -1,6 +1,26 @@
 
 issues = document.querySelector("#issues-tab > span:nth-child(2)")
 
-alert("Hello from your Chrome extension!", `${issues.text}`)
+// fetch(url).then(r=> r.json().then(j=> console.log('\nREQUEST 2',j)));
 
 issues.textContent= "lol it works!"
+
+
+function goodIssue(issue) {
+  return issue.labels.find(l => l.name == "good first issue").length > 0;
+}
+
+url = window.location.toString().split("/")
+user = url[3]
+repo = url[4]
+
+base = "https://api.github.com/repos/"
+
+async function funcName(url){
+    const response = await fetch(url);
+    var data = await response.json();
+}
+
+cnt = funcName(base + user + "/" + repo).find(goodIssue);
+alert(`cnt is ${cnt}`)
+
