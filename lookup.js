af93899c10b0c2d0258a555de3a89ad13bf9e89b
Ravil Galeyev <dehasi@yandex.ru>
2021-09-13 01:13:00 +0200

    update page

--- lookup.js
+++ lookup.js
@@ -1,3 +1,6 @@
 
-alert("Hello from your Chrome extension!")
+issues = document.querySelector("#issues-tab > span:nth-child(2)")
 
+alert("Hello from your Chrome extension!", `${issues.text}`)
+
+issues.textContent= "lol it works!"
