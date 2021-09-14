e43a4c8362f65d83a4b855b833cc2b1565649802
Ravil Galeyev <dehasi@yandex.ru>
2021-09-15 00:08:45 +0200

    X

--- lookup.js
+++ lookup.js
@@ -1,30 +1,31 @@
-
-issues = document.querySelector("#issues-tab > span:nth-child(2)")
+issues = document.querySelector('#issues-tab > span:nth-child(2)');
 
 if (issues == null) {
-    console.log('No Issues tab')
-    return
+  console.log('No Issues tab');
+  // return;
 }
 // fetch(url).then(r=> r.json().then(j=> console.log('\nREQUEST 2',j)));
 
-issues.textContent= "lol it works!"
+issues.textContent = 'lol it works!';
 
+function requestIssues() {
+  url = window.location.toString().split('/');
+  user = url[3];
+  repo = url[4];
+  base = `https://api.github.com/repos/${user}/${repo}`;
+
+
+  fetch(base + user + '/' + repo).then(r => r.json());
+}
 
 function goodIssue(issue) {
-  return issue.labels.find(l => l.name == "good first issue").length > 0;
+  return issue.labels.find(l => l.name === 'good first issue').length > 0;
 }
 
-url = window.location.toString().split("/")
-user = url[3]
-repo = url[4]
-
-base = "https://api.github.com/repos/"
-
-async function funcName(url){
-    const response = await fetch(url);
-    var data = await response.json();
+async function funcName(url) {
+  const response = await fetch(url);
+  var data = await response.json();
 }
 
-cnt = funcName(base + user + "/" + repo).find(goodIssue);
-alert(`cnt is ${cnt}`)
-
+cnt = funcName(base + user + '/' + repo).find(goodIssue);
+alert(`cnt is ${cnt}`);
