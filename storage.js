27ac651bce12b5a38be87ca49958368b37c35e48
Ravil Galeyev <dehasi@yandex.ru>
2023-02-22 18:51:15 +0100

    FIX empty object

--- storage.js
+++ storage.js
@@ -11,15 +11,18 @@ export default class Storage {
   }
 
   async labels() {
-    const defaultLabels = ['bug', 'good first issue']
     const labelSet = await this.loadLabels();
+    const result = Array.from(labelSet).sort();
 
-    if (labelSet == null) {
+    if (result.length == 0) {
+      const defaultLabels = ['bug', 'good first issue']
+      console.log('Saving defaultLabels: ' + defaultLabels);
+
       this.saveLabels(defaultLabels);
       return defaultLabels;
     }
 
-    return Array.from(labelSet).sort();
+    return result;
   }
 
   async loadLabels() {
